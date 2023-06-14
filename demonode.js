a43458cabe0335e8adb1953694ede60f8c232702
mahakgwalera <mgwalera@gmail.com>
2023-06-15 00:18:06 +0530

    first commit

---
+++ demonode.js
@@ -0,0 +1,242 @@
+/*console.log("hello");
+var fname="js"
+console.log(fname);
+var n1=5
+var n2=8
+var r=n1+n2
+console.log(r);
+
+var r=n1-n2
+console.log(r);
+
+var r=n1*n2
+console.log(r);
+
+var r=n1/n2
+console.log(r);
+
+var n1 =5
+n1=n1+5
+console.log(n1);
+
+n1=n1-5
+console.log(n1);
+
+n1=n1*2
+console.log(n1);
+
+n1=n1/3
+console.log(n1);
+
+n1=123
+n1=n1%10
+console.log(n1);
+
+var n1=5
+var n2=8
+var r=n1>n2
+console.log(r);
+
+var r=n1<n2
+console.log(r);
+
+var r=n1>=n2
+console.log(r);
+
+var r=n1<=n2
+console.log(r);
+
+var r=n1!=n2
+console.log(r);
+
+var r=n1!==n2
+console.log(r);
+
+var r=n1==n2
+console.log(r);
+
+var r=n1===n2
+console.log(r);
+
+ var n1 =50
+ var n2 =4
+ var r=n1>10 && n2<20
+ console.log(r);
+
+ var n1 =5
+ var n2 =4
+ var r=n1>10 || n2<20
+ console.log(r);
+
+ var n1 =5
+ var n2 =4
+ var r= !(n1>10 || n2<20)
+ console.log(r);
+
+var n1=3
+n1++
+console.log(n1); 
+
+var n1=3
+++n1
+console.log(n1); 
+
+var n1=3
+n1--
+console.log(n1); 
+
+(3)? console.log("true"):console.log("false");
+(-8)? console.log("true"):console.log("false");
+("mahak")? console.log("true"):console.log("false");
+(0)? console.log("true"):console.log("false");
+(false)? console.log("true"):console.log("false");
+
+var name1="mahak"
+
+console.log(name1.length);
+name1 && console.log(name1);
+
+
+
+if(true){
+    console.log("login");
+}
+if(false){
+    console.log("login");
+}
+pin=2224
+if(pin==2222){  //true pin
+    console.log("login");
+}
+else if(pin==12435){
+    //true password
+    console.log("login");
+}
+else{
+    //false
+    console.log("not e login")
+}
+
+
+email="user@gmail.com"
+if(email=="Admin@gmail.com"){
+    password=1234
+    if(password==1222){
+        console.log("login");
+    }
+}
+else{
+    console.log("not login");
+}
+
+//condition
+
+ch=1
+switch(ch){
+    case 1:console.log("add");
+    break;
+
+    case 2:console.log("sub");
+    break;
+
+    case 3:console.log("div");
+    break;
+
+    default:console.log("not use");
+
+}
+
+//loop
+
+for(i=1;i<=10;i++){
+    console.log("hello");
+}
+t=2
+for(i=1;i<=10;i++){
+    console.log(t,"*",i,"=",i*t);
+}
+for(i=1;i<=10;i++){
+    if(i%2==0){
+    console.log(i);
+    }
+    else{
+        console.log(i);
+    }
+
+}
+
+
+
+i=11
+while(i<=10){
+
+    console.log(i);
+    i++
+}
+i=1
+while(i<=10){
+
+    console.log(i);
+    i++
+}
+
+i=11
+do{
+
+    console.log(i);
+    i++
+}while(i<=10)
+
+num1 = 7
+
+num1 = [2,3,4,5] //array
+
+num1 ="1"
+
+num1 = ["user1","user2","user3"] //0,1,2 position
+
+num1[0]="user1"
+num1[1]="user2"
+num1[2]="user3"
+
+num=1234
+
+console.log(num);
+
+for(i=0;i<5;i++){
+    console.log(num[i]);
+}*/
+
+
+num = 23256
+console.log("num=",num);
+num1=num
+count=0
+A=[]
+while(num!=0){
+    count++
+    num=parseInt(num/10)
+}
+console.log("***************");
+console.log(count);
+count1=count--
+while(num!=0){
+    A[count]=num1%10
+
+count--
+num1=parseInt(num1/10)
+}
+console.log("***************");
+console.log(A);
+/*for(i=0;i<count+1;i++){
+    console.log(A[i]);
+}
+
+
+item=[3,2,6,5,8]
+sum=0
+for(i=0;i<item.length;i++)
+{
+
+}*/
+
